1d4bca8ebe48bda45d5708202bf6fc1080cc0ab7
Roland Ramadan <rolandramadan25@gmail.com>
2023-06-07 15:21:02 +0700

    Header Module

---
+++ GoVege/GoVege/View/MasterPage.Master.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GoVege.View
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnCart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
