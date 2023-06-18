c14461ef6d7736aaf17e4d16f37597339ee666eb
prisley03 <prillysci.laindra@gmail.com>
2023-06-18 20:29:26 +0700

    PromotionPage blm kelar

---
+++ GoVege/GoVege/Controller/PromotionController.cs
@@ -0,0 +1,16 @@
+﻿using GoVege.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoVege.Controller
+{
+    public class PromotionController
+    {
+        public static List<MsVoucher> getAllPromotion()
+        {
+            return PromotionRepository.getAllPromotion();
+        }
+    }
+}
---
+++ GoVege/GoVege/Repository/PromotionRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoVege.Repository
+{
+    public class PromotionRepository
+    {
+            static GoVegeDBEntities db = new GoVegeDBEntities();
+            public static List<MsVoucher> getAllPromotion()
+            {
+                return db.MsVouchers.ToList();
+            }
+
+            public MsVoucher GetPromotionsById(int voucherId)
+            {
+                using (var context = new GoVegeDBEntities())
+                {
+                    return context.MsVouchers.FirstOrDefault(v => v.voucherID == voucherId);
+                }
+            }
+    }
+}
