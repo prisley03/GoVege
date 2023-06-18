3afa308a714018c7fde1a50f43ee698fe1065690
Matthew Avrillio <matthew.avrillio@binus.ac.id>
2023-06-18 20:00:34 +0700

    Update SQL for Insert Voucher, Create Transaction, Cart, TransactionDetails

---
+++ GoVege/GoVege/Repository/VendorRepository.cs
@@ -0,0 +1,17 @@
+﻿using GoVege.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoVege.Repository
+{
+    public class VendorRepository
+    {
+        public static GoVegeDBEntities db = DatabaseSingleton.GetInstance();
+        public static MsVendor GetVendorByID(int id)
+        {
+            return (from i in db.MsVendors where i.vendorID == id select i).FirstOrDefault();
+        }
+    }
+}
---
+++ GoVege/GoVege/View/StorePage.aspx.cs
@@ -0,0 +1,61 @@
+﻿using GoVege.Model;
+using GoVege.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GoVege.View
+{
+    public partial class StorePage : System.Web.UI.Page
+    {
+        public MsVendor vendorTarget = null;
+        public List<MsProduct> productList = null;
+        public int vendorID;
+        public double vendorRating;
+        public int fullStarCount, halfStarCount, emptyStarCount;
+
+        protected void BtnAddCart_Command(object sender, CommandEventArgs e)
+        {
+            if(e.CommandArgument == null)
+            {
+                Response.Redirect("~/View/HomePage.aspx");
+            }
+            else
+            {
+                
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["VendorID"] == null)
+            {
+                Response.Redirect("~/View/HomePage.aspx");
+            }
+            vendorID = Convert.ToInt32(Request.QueryString["VendorID"]);
+
+            vendorTarget = VendorRepository.GetVendorByID(vendorID);
+            vendorRating = vendorTarget.vendorRating;
+            
+
+            fullStarCount = (int)(vendorRating / 1);
+            halfStarCount = (int)((vendorRating - fullStarCount) / 0.5);
+            emptyStarCount = (int)(5 - (fullStarCount + halfStarCount));
+
+            if (vendorTarget == null)
+            {
+                Response.Redirect("~/View/HomePage.aspx");
+            }
+
+            String vendorImage = vendorTarget.vendorImage;
+            ImageVendor.ImageUrl = "~/Assets/Vendor/" + vendorImage;
+            
+            productList = ProductRepository.GetProductsByVendorID(vendorID);
+            ListViewProduct.DataSource = productList;
+            ListViewProduct.DataBind();
+        }
+    }
+}
